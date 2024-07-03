cfcb9f516aa9d1a7efa53f6622b99efc53795190
0xArthem <82900721+0xArthem@users.noreply.github.com>
2024-07-03 17:10:42 +0200

    10 - Création d'un CRUD pour les équipements

---
+++ src/Form/EquipmentType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Equipment;
+use App\Entity\Test;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EquipmentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer l\'équipment'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Equipment::class,
+        ]);
+    }
+}
