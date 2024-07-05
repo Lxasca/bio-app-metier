65906cc21d27dd59f0e61cf3711e5b9c6bda23e3
0xArthem <82900721+0xArthem@users.noreply.github.com>
2024-07-05 22:11:27 +0200

    24 - Mise en forme du formulaire d'édition de Result

--- src/Form/ResultType.php
+++ src/Form/ResultType.php
@@ -7,6 +7,7 @@ use App\Entity\Test;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,12 +16,24 @@ class ResultType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('observation')
+            ->add('observation', TextareaType::class, [
+                'label' => false,
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ]
+            ])
             ->add('test', EntityType::class, [
+                'label' => false,
                 'class' => Test::class,
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ]
             ])
             ->add('submit', SubmitType::class, [
-                'label' => 'Enregistrer le résultat'
+                'label' => 'Enregistrer',
+                'attr' => [
+                    'class' => 'btn btn-rounded btn-light shadow mt-3'
+                ]
             ])
         ;
     }
