b0a6d4bd4dc03393f18d485f10154b71a3933877
0xArthem <82900721+0xArthem@users.noreply.github.com>
2024-07-05 21:59:06 +0200

    21 - Mise en forme du formulaire d'édition de Test

--- src/Form/TestType.php
+++ src/Form/TestType.php
@@ -8,6 +8,8 @@ use App\Entity\Test;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,16 +18,33 @@ class TestType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('scenario')
-            ->add('submit', SubmitType::class, [
-                'label' => 'Enregistrer l\'échantillon'
+            ->add('title', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ]
+            ])
+            ->add('scenario', TextareaType::class, [
+                'label' => false,
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ]
             ])
             ->add('equipments', EntityType::class, [
+                'label' => false,
                 'class' => Equipment::class,
                 'choice_label' => 'name',
                 'multiple' => true,
                 'expanded' => true,
+                'attr' => [
+                    'class' => 'form-checkbox-group mb-3'
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer',
+                'attr' => [
+                    'class' => 'btn btn-rounded btn-light shadow mt-3'
+                ]
             ])
         ;
     }
